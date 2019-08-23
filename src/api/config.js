703a96b4d5d6769b2dc685ff82bfb958eef118e0
Olin Fernandes <olin.fernandes@ninestack.com>
2019-08-23 18:22:16 +0530

    Updated

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,50 @@
+import mongoose from "mongoose";
+
+const Schema = new mongoose.Schema();
+const todoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    index: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: Date,
+    default: Date.now()
+  },
+  created_by: {
+    type: String,
+    required: true,
+    index: true
+  }
+});
+const userSchema = new Schema({
+  name: {
+    firstName: {
+      type: String
+    },
+    lastName: {
+      type: String
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    index: true,
+    uniquer: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export const Todo = mongoose.model("Todo", todoSchema);
+export const User = mongoose.model("User", userSchema);
+
 export const abortController = new AbortController();
 export const signal = abortController.signal;
 
