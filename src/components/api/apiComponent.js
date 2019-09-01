51f0d279564e3b1207a489d7801fd4e1eb4efb93
Olin Fernandes <olin.fernandes@ninestack.com>
2019-09-01 19:31:46 +0530

    Updated

--- src/components/api/apiComponent.js
+++ src/components/api/apiComponent.js
@@ -3,10 +3,10 @@ import { setOptions, api_routes } from "./config";
 const userApi = api_routes["users"];
 const todoApi = api_routes["todos"];
 
-// GET Requests - api calls
+// Collection ops api calls
 
 // GET users listings
-export async function getUsers(callback = Function) {
+export async function getUsers(callback = Function()) {
   try {
     const response = await fetch(`${userApi}`, setOptions());
     const dataList = response.json();
@@ -16,15 +16,131 @@ export async function getUsers(callback = Function) {
     return callback(error);
   }
 }
-
 // GET todos listings
-export async function getTodos(callback = Function) {
+export async function getTodos(callback = Function()) {
   try {
     const response = await fetch(`${todoApi}`, setOptions());
     const dataList = response.json();
     return callback(null, dataList);
   } catch (error) {
-    console.log(error);
+    return callback(error);
+  }
+}
+
+// POST users listings
+export async function addUsers(dataList = [], callback = Function()) {
+  try {
+    const response = await fetch(
+      `${userApi}`,
+      setOptions({ method: "POST", body: dataList })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+// POST todos listings
+export async function addTodos(dataList = [], callback = Function()) {
+  try {
+    const response = await fetch(
+      `${todoApi}`,
+      setOptions({ method: "POST", body: dataList })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+
+// Document ops api calls
+
+// GET user document
+export async function getUserById(id = String(), callback = Function()) {
+  try {
+    const response = await fetch(`${userApi}/${id}`, setOptions());
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+// GET todo document
+export async function getTodoById(id = String(), callback = Function()) {
+  try {
+    const response = await fetch(`${todoApi}/${id}`, setOptions());
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+
+// PUT user document
+export async function updateUserById(
+  id = String(),
+  userUpdates = {},
+  callback = Function()
+) {
+  try {
+    const response = await fetch(
+      `${userApi}/${id}`,
+      setOptions({
+        method: "PUT",
+        body: userUpdates
+      })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+// PUT todo document
+export async function updateTodoById(
+  id = String(),
+  todoUpdates = {},
+  callback = Function()
+) {
+  try {
+    const response = await fetch(
+      `${todoApi}/${id}`,
+      setOptions({
+        method: "PUT",
+        body: todoUpdates
+      })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+
+// DELETE user document
+export async function deleteUserById(id = String(), callback = Function()) {
+  try {
+    const response = await fetch(
+      `${userApi}/${id}`,
+      setOptions({ method: "DELETE" })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
+}
+// DELETE todo document
+export async function deleteTodoById(id = String(), callback = Function()) {
+  try {
+    const response = await fetch(
+      `${todoApi}/${id}`,
+      setOptions({ method: "DELETE" })
+    );
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
     return callback(error);
   }
 }
--- src/components/api/authComponent.js
+++ src/components/api/authComponent.js
@@ -4,33 +4,36 @@ const authApi = api_routes["auth"];
 
 // Logout User - GET api call
 export async function logotUser(callback = Function) {
-    try {
-        const response = await fetch(`${authApi}/logout`, setOptions());
-        const data = response.json();
-        return callback(null, data);
-    } catch (error) {
-        return callback(error);
-    }
+  try {
+    const response = await fetch(`${authApi}/logout`, setOptions());
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
 }
 
 // IsLoggedIn User - GET api call
 export async function isloggedin(callback = Function) {
-    try {
-        const response = await fetch(`${authApi}/isloggedin`, setOptions());
-        const data = response.json();
-        return callback(null, data);
-    } catch (error) {
-        return callback(error);
-    }
+  try {
+    const response = await fetch(`${authApi}/isloggedin`, setOptions());
+    const data = response.json();
+    return callback(null, data);
+  } catch (error) {
+    return callback(error);
+  }
 }
 
 // Login User - POST api call
 export async function loginUser(user = {}, callback = Function) {
   try {
-    const response = await fetch(`${authApi}/login`, setOptions({
-      method: 'POST',
-      query: user
-    }));
+    const response = await fetch(
+      `${authApi}/login`,
+      setOptions({
+        method: "POST",
+        body: user
+      })
+    );
     const data = response.json();
     return callback(null, data);
   } catch (error) {
@@ -41,10 +44,13 @@ export async function loginUser(user = {}, callback = Function) {
 // Add/Register User - POST api call
 export async function register(user = {}, callback = Function) {
   try {
-    const response = await fetch(`${authApi}/register`, setOptions({
-      method: 'POST',
-      query: user
-    }));
+    const response = await fetch(
+      `${authApi}/register`,
+      setOptions({
+        method: "POST",
+        body: user
+      })
+    );
     const data = response.json();
     return callback(null, data);
   } catch (error) {
