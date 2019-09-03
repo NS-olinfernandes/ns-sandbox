f722a9563f2d9590f865b8879e6c80063670655d
Olin Fernandes <olin.f@live.com>
2019-09-03 16:01:43 +0530

    Updated

--- src/routes/api/_models.js
+++ src/routes/api/_models.js
@@ -1,67 +1,66 @@
-import mongoose from "mongoose";
-// const db_url = "mongodb/Sanbox";
-const db_url = "ns-mongodb1/Sandbox";
+import mongoose from 'mongoose';
+const db_url = 'ns-mongodb1/Sandbox';
 // const db_url = "localhost/Sandbox";
 // const db_url = "172.18.0.2/Sandbox";
 
 // MongoDB connection.
 mongoose.connect(`mongodb://${db_url}`, {
-  useCreateIndex: true,
-  useNewUrlParser: true
+	useCreateIndex: true,
+	useNewUrlParser: true
 });
-mongoose.connection.on("connected", () =>
-  console.log("Connected to Sanbox Database")
+mongoose.connection.on('connected', () =>
+	console.log('Connected to Sanbox Database')
 );
 
 const Schema = mongoose.Schema;
 // Todo model Schema
 const todoSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    index: true
-  },
-  body: {
-    type: String,
-    default: "",
-    required: true
-  },
-  created_at: {
-    type: Date,
-    default: Date.now()
-  },
-  created_by: {
-    type: String,
-    required: true,
-    index: true
-  }
+	title: {
+		type: String,
+		required: true,
+		index: true
+	},
+	body: {
+		type: String,
+		default: '',
+		required: true
+	},
+	created_at: {
+		type: Date,
+		default: Date.now()
+	},
+	created_by: {
+		type: String,
+		required: true,
+		index: true
+	}
 });
 // User model Schema
 const userSchema = new Schema({
-  name: {
-    firstName: {
-      type: String,
-      default: ""
-    },
-    lastName: {
-      type: String,
-      default: ""
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    index: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  accessToken: {
-    type: String
-  }
+	name: {
+		firstName: {
+			type: String,
+			default: ''
+		},
+		lastName: {
+			type: String,
+			default: ''
+		}
+	},
+	email: {
+		type: String,
+		required: true,
+		index: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	accessToken: {
+		type: String
+	}
 });
 
-export const Todo = mongoose.model("Todo", todoSchema);
-export const User = mongoose.model("User", userSchema);
+export const Todo = mongoose.model('Todo', todoSchema);
+export const User = mongoose.model('User', userSchema);
